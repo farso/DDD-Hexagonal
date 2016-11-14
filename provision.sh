9f05898b3f7fbd357773088c94221cac6a9e56c9
Israel Fores <ifores@uic.es>
2016-11-14 14:11:53 +0100

    provision amb jenkins

--- provision.sh
+++ provision.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 #echo "///////////////////////////////////////////////"
-#echo "Updating canonical repositories..."
+#echo "Installing Jenkings CI"
 #echo "///////////////////////////////////////////////"
 #apt-get update > /dev/null
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins --assume-yes
 
 #echo "///////////////////////////////////////////////"
 #echo "Installing apache2..."
